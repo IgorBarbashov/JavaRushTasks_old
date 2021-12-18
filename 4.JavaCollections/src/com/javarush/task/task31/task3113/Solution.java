ed26018f5a04ce736866753d1b1d41d2248b219b
Igor Barbashov <bis200705@gmail.com>
2021-12-18 10:43:28 +0300

    feat: jr-variant task31.task3113

--- 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -1,64 +1,51 @@
 package com.javarush.task.task31.task3113;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.EnumSet;
-import java.util.Scanner;
-
-import static java.lang.Integer.MAX_VALUE;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /* 
 Что внутри папки?
 */
 
-public class Solution extends SimpleFileVisitor<Path> {
+public class Solution {
 
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
-        String pathName = in.nextLine();
-        Path rootPath = Paths.get(pathName);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String dirName = reader.readLine();
 
-        if (!Files.isDirectory(rootPath)) {
-            System.out.println(pathName + " - не папка");
-            return;
+        Path directory = Paths.get(dirName);
+
+        if (!Files.isDirectory(directory)) {
+            System.out.println(directory.toString() + " - не папка.");
+        } else {
+            AtomicInteger folderCounter = new AtomicInteger();
+            AtomicInteger fileCounter = new AtomicInteger();
+            AtomicLong sizeCounter = new AtomicLong();
+
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    if (!dir.equals(directory)) folderCounter.incrementAndGet();
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    fileCounter.incrementAndGet();
+                    sizeCounter.addAndGet(attrs.size());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+            System.out.println("Параметры папки " + directory.toString() + ":");
+            System.out.println("Всего папок - " + folderCounter.get());
+            System.out.println("Всего файлов - " + fileCounter.get());
+            System.out.println("Общий размер - " + sizeCounter.get() + " байт");
         }
-
-        EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
-        final Solution fileVisitor = new Solution();
-        Files.walkFileTree(rootPath, options, MAX_VALUE, fileVisitor);
-
-        int foldersCount = fileVisitor.getFoldersCount();
-        int filesCount = fileVisitor.getFilesCount();
-        int bytesCount = fileVisitor.getBytesCount();
-
-        System.out.println("Всего папок - " + (foldersCount - 1));
-        System.out.println("Всего файлов - " + filesCount);
-        System.out.println("Общий размер - " + bytesCount);
-    }
-
-    private int foldersCount;
-    private int filesCount;
-    private int bytesCount; // общее количество байт, которое хранится в директории
-
-    public int getFoldersCount() { return this.foldersCount; }
-    public int getFilesCount() { return this.filesCount; }
-    public int getBytesCount() { return this.bytesCount; }
-
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes basicFileAttributes) throws IOException {
-        if (Files.isRegularFile(file)) {
-            this.filesCount ++;
-            this.bytesCount += Files.readAllBytes(file).length;
-        }
-        return super.visitFile(file, basicFileAttributes);
-    }
-
-    @Override
-    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-        if (Files.isDirectory(dir)) {
-            this.foldersCount++;
-        }
-        return super.postVisitDirectory(dir, exc);
     }
 }
